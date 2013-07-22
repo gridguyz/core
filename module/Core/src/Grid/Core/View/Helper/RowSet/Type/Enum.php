d524afa462da9c46df5ebfa5df78dce89246c697
David Pozsar <david.pozsar@megaweb.hu>
2013-07-22 15:07:51 +0200

    rowSet: fix set & enum rendering

--- module/Core/src/Grid/Core/View/Helper/RowSet/Type/Enum.php
+++ module/Core/src/Grid/Core/View/Helper/RowSet/Type/Enum.php
@@ -4,17 +4,22 @@ namespace Grid\Core\View\Helper\RowSet\Type;
 
 /**
  * Enum
- * 
+ *
  * @author David Pozsar <david.pozsar@megaweb.hu>
  */
 class Enum extends Translate
 {
-    
+
     /**
      * @var array
      */
     protected $values = array();
-    
+
+    /**
+     * @var bool
+     */
+    protected $translationEnabled = true;
+
     /**
      * @return array
      */
@@ -22,42 +27,63 @@ class Enum extends Translate
     {
         return $this->values;
     }
-    
+
     /**
-     * @param array $values
-     * @return \Core\View\Helper\RowSet\Type\Enum
+     * @param   array $values
+     * @return  \Grid\Core\View\Helper\RowSet\Type\Enum
      */
     public function setValues( array $values )
     {
         $this->values = $values;
         return $this;
     }
-    
+
+    /**
+     * @return array
+     */
+    public function getTranslationEnabled()
+    {
+        return $this->translationEnabled;
+    }
+
+    /**
+     * @param   bool $translationEnabled
+     * @return  \Grid\Core\View\Helper\RowSet\Type\Enum
+     */
+    public function setTranslationEnabled( $translationEnabled )
+    {
+        $this->translationEnabled = (bool) $translationEnabled;
+        return $this;
+    }
+
     /**
      * Create enumeration
-     * 
-     * @param array $values
-     * @param string $translatePrefix
-     * @param string $translatePostfix
-     * @param string $translateTextDomain
+     *
+     * @param   array   $values
+     * @param   string  $translatePrefix
+     * @param   string  $translatePostfix
+     * @param   string  $translateTextDomain
+     * @param   bool    $translationEnabled
      */
     public function __construct( array $values          = array(),
                                  $translatePrefix       = null,
                                  $translatePostfix      = null,
-                                 $translateTextDomain   = null )
+                                 $translateTextDomain   = null,
+                                 $translationEnabled    = true )
     {
         parent::__construct(
             $translatePrefix,
             $translatePostfix,
             $translateTextDomain
         );
-        
-        $this->setValues( $values );
+
+        $this->setValues( $values )
+             ->setTranslationEnabled( $translationEnabled );
     }
-    
+
     /**
      * Display a single value
-     * 
+     *
      * @param string $value
      * @return string
      */
@@ -67,8 +93,13 @@ class Enum extends Translate
         {
             $value = $this->values[$value];
         }
-        
-        return parent::displayValue( $value );
+
+        if ( $this->translationEnabled )
+        {
+            $value = parent::displayValue( $value );
+        }
+
+        return $value;
     }
-    
+
 }
--- module/Core/src/Grid/Core/View/Helper/RowSet/Type/Set.php
+++ module/Core/src/Grid/Core/View/Helper/RowSet/Type/Set.php
@@ -9,12 +9,17 @@ namespace Grid\Core\View\Helper\RowSet\Type;
  */
 class Set extends Enum
 {
-    
+
     /**
      * @var string
      */
     protected $separator = ', ';
-    
+
+    /**
+     * @var string
+     */
+    protected $splitter = "\n";
+
     /**
      * @return string
      */
@@ -22,57 +27,90 @@ class Set extends Enum
     {
         return $this->separator;
     }
-    
+
     /**
-     * @param string $separator
-     * @return \Core\View\Helper\RowSet\Type\Set
+     * @param   string $separator
+     * @return  \Grid\Core\View\Helper\RowSet\Type\Set
      */
     public function setSeparator( $separator )
     {
         $this->separator = (string) $separator;
         return $this;
     }
-    
+
+    /**
+     * @return string
+     */
+    public function getSplitter()
+    {
+        return $this->splitter;
+    }
+
+    /**
+     * @param   string $splitter
+     * @return  \Grid\Core\View\Helper\RowSet\Type\Set
+     */
+    public function setSplitter( $splitter )
+    {
+        $this->splitter = (string) $splitter;
+        return $this;
+    }
+
     /**
      * Create enumeration
-     * 
-     * @param array $values
-     * @param string $translatePrefix
-     * @param string $translatePostfix
-     * @param string $translateTextDomain
-     * @param string $separator
+     *
+     * @param   array   $values
+     * @param   string  $translatePrefix
+     * @param   string  $translatePostfix
+     * @param   string  $translateTextDomain
+     * @param   string  $separator
+     * @param   bool    $translationEnabled
+     * @param   string  $splitter
      */
     public function __construct( array $values          = array(),
                                  $translatePrefix       = null,
                                  $translatePostfix      = null,
                                  $translateTextDomain   = null,
-                                 $separator             = null )
+                                 $separator             = null,
+                                 $translationEnabled    = true,
+                                 $splitter              = null )
     {
         parent::__construct(
             $values,
             $translatePrefix,
             $translatePostfix,
-            $translateTextDomain
+            $translateTextDomain,
+            $translationEnabled
         );
-        
+
         if ( null !== $separator )
         {
             $this->setSeparator( $separator );
         }
+
+        if ( null !== $splitter )
+        {
+            $this->setSplitter( $splitter );
+        }
     }
-    
+
     /**
      * Display a single value
-     * 
-     * @param string|array $value
-     * @return string
+     *
+     * @param   string|array $value
+     * @return  string
      */
     public function displayValue( $values )
     {
+        if ( is_scalar( $values ) && ! empty( $this->splitter ) )
+        {
+            $values = explode( $this->splitter, $values );
+        }
+
         $result = '';
         $first  = true;
         $values = (array) $values;
-        
+
         foreach ( $values as $value )
         {
             if ( $first )
@@ -83,11 +121,11 @@ class Set extends Enum
             {
                 $result .= $this->separator;
             }
-            
+
             $result .= parent::displayValue( $value );
         }
-        
+
         return $result;
     }
-    
+
 }
