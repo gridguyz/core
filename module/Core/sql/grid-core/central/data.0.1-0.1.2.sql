cc89de87bb2bba31fc9746ab712c9966cd23ddee
David Pozsar <david.pozsar@megaweb.hu>
2013-09-18 09:57:58 +0200

    patch for default layouts (fix default menus)

--- module/Core/sql/grid-core/central/data.0.1-0.1.2.sql
+++ module/Core/sql/grid-core/central/data.0.1-0.1.2.sql
@@ -4,3 +4,9 @@ UPDATE "_central"."paragraph_property"
    SET "value"  = REPLACE( "value", 'All rights reserved! 2012 GridZ', 'Powered by GridGuyz' )
  WHERE "locale" <> '*'
    AND "name"   = 'html';
+
+UPDATE "_central"."paragraph_property"
+   SET "value"  = '1'
+ WHERE "locale" = '*'
+   AND "name"   = 'menuId'
+   AND "value"  <> '1';
