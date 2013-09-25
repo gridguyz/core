674a28bee792577918499953eb53ddc9bcc4f388
David Pozsar <david.pozsar@megaweb.hu>
2013-09-25 14:00:09 +0200

    rowSet: format int & float columns according to the current locale

--- module/Core/src/Grid/Core/View/Helper/RowSet/Type/Float.php
+++ module/Core/src/Grid/Core/View/Helper/RowSet/Type/Float.php
@@ -4,21 +4,25 @@ namespace Grid\Core\View\Helper\RowSet\Type;
 
 /**
  * Float
- * 
+ *
  * @author David Pozsar <david.pozsar@megaweb.hu>
  */
 class Float implements TypeInterface
 {
-    
+
     /**
      * Display a single value
-     * 
+     *
      * @param float $value
      * @return string
      */
     public function displayValue( $value )
     {
-        return (string) (float) $value;
+        return $this->view->numberFormat(
+            (float) $value,
+            NumberFormatter::DECIMAL,
+            NumberFormatter::TYPE_DOUBLE
+        );
     }
-    
+
 }
